8fd71a0cadc42aa8ab643b3950094dea51312aba
adarshswaminath <adarshswaminath7@gmail.com>
2024-03-11 00:46:45 +0530

    feat: tender page multi file listing added

--- src/Pages/Tender/TenderTable.jsx
+++ src/Pages/Tender/TenderTable.jsx
@@ -1,4 +1,4 @@
-import { FaExternalLinkAlt } from "react-icons/fa";
+import { FaExternalLinkAlt, FaTimes } from "react-icons/fa";
 import { useState } from "react";
 import useGetApi from "../../Hook/useGetApi";
 import Loading from "../../Components/Loading";
@@ -6,11 +6,13 @@ import { useLanguage } from "../../contexts/LanguageContext";
 
 const TenderTable = () => {
   const { response } = useGetApi("tenders");
-  const {language} = useLanguage()
+  const { language } = useLanguage();
 
   // Move the hook calls to the top of the component function
   const [filter, setFilter] = useState("all");
   const [subFilter, setSubFilter] = useState("all");
+  const [modalOpen, setModalOpen] = useState(false);
+  const [selectedFiles, setSelectedFiles] = useState([]);
 
   if (!response) {
     return <Loading />;
@@ -39,99 +41,71 @@ const TenderTable = () => {
     setSubFilter(newSubFilter);
   };
 
+  const handleOpenModal = (files) => {
+    setSelectedFiles(files);
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedFiles([]);
+    setModalOpen(false);
+  };
+
   return (
     <div>
-      <div className="flex items-center justify-center space-x-4 mb-4">
-        <button
-          className={`btn ${filter === "all" && "btn-active bg-red-500 text-white"}`}
-          onClick={() => handleFilterChange("all")}
-        >
-          {language ? "All" : "എല്ലാം"}
-        </button>
-        <button
-          className={`btn ${filter === "live" && "btn-active bg-red-500 text-white"}`}
-          onClick={() => handleFilterChange("live")}
-        >
-          {language ? "Live Tenders" : "ലൈവ് ടെന്‍ഡര്‍സ്"}
-        </button>
-        <button
-          className={`btn ${filter === "previous" && "btn-active bg-red-500 text-white"}`}
-          onClick={() => handleFilterChange("previous")}
-        >
-          {language ? "Previous Tenders" : "മുന്‍നിര ടെന്‍ഡര്‍സ്"}
-        </button>
-      </div>
-      <div className="flex items-center justify-center">
-        {filter === "live" && (
-          <div className="flex items-center space-x-2">
-            <button
-              className={`btn ${subFilter === "all" && "btn-active bg-red-500 text-white"}`}
-              onClick={() => handleSubFilterChange("all")}
-            >
-              {language ? "All" : "എല്ലാം"}
-            </button>
-            <button
-              className={`btn ${subFilter === "Tenders" && "btn-active bg-red-500 text-white"}`}
-              onClick={() => handleSubFilterChange("Tenders")}
-            >
-              {language ? "Tenders" : "ടെൻഡർ"}
-            </button>
-            <button
-              className={`btn ${subFilter === "E-Tenders" && "btn-active bg-red-500 text-white"}`}
-              onClick={() => handleSubFilterChange("E-Tenders")}
-            >
-                        <span>{language ? "E-Tenders" : "ഇ-ടെൻഡർ"}</span>
-            </button>
-          </div>
-        )}
-      </div>
-      <div className="flex items-center justify-center gap-3 m-3">
-        <>
-          <span className="badge badge-xs bg-green-500"></span>
-          <span>{language ? "Tender" : "ടെൻഡർ"}</span>
-        </>
-        <>
-          <span className="badge badge-xs bg-red-500"></span>
-          <span>{language ? "E-Tender" : "ഇ-ടെൻഡർ"}</span>
-        </>
-      </div>
       <div className="overflow-x-auto">
         <table className="table">
           <thead>
-            <tr className="text-black bg-red-400">
-              <th></th>
+            <tr>
               <th>{language ? "Tender Title" : "ടെണ്ടർ തലക്കെട്ട്"}</th>
-              <th>{language ? "Date" : "തീയതി"}</th>
-              {filter === "previous" && <th>Expiry Date</th>}
+              <th>{language ? "Expiry Date" : "കാലാവധി തീയതി"}</th>
+              <th>Action</th>
             </tr>
           </thead>
           <tbody>
             {filteredData.map((value, index) => (
-              <tr className={`${index % 2 === 0 ? "bg-red-100" : "bg-red-50"}`} key={index}>
-                <th>{index + 1}</th>
-                <td className="font-bold flex items-center space-x-5">
-                  {value.files && value.files.length > 0 && value.files[0].file ? (
-                      <a href={value.files[0].file} target="_blank" rel="noopener noreferrer">
-                        {language ? value.title_en : value.title_ml}
-                      </a>
-                      ) : (
-                        <span>{language ? value.title_en : value.title_ml}</span>
-                        )}
-
-                  {value.is_e_tender && (
-                    <span className="badge badge-xs bg-red-500 p-2">E-Tender</span>
-                  )}
-                  {!value.is_e_tender && (
-                    <span className="badge badge-xs bg-green-500 p-2">Tender</span>
-                  )}
+              <tr key={index} className={`${index %2 ===0 ? "bg-red-200" : "bg-red-100"}`}>
+                <td>{language ? value.title_en : value.title_ml}</td>
+                <td>{value.expiry_date}</td>
+                <td>
+                  <button
+                    className="btn bg-red-300 border-none"
+                    onClick={() => handleOpenModal(value.files)}
+                  >
+                    Open Docs
+                  </button>
                 </td>
-                <td>{value.published_date}</td>
-                {filter === "previous" && <td>{value.expiry_date}</td>}
               </tr>
             ))}
           </tbody>
         </table>
       </div>
+      {/* Modal */}
+      {modalOpen && (
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-50 z-50">
+          <div className="bg-white p-4 rounded-lg min-h-72 w-96  max-w-md">
+            <div className="flex justify-between items-center mb-4">
+              <h2 className="text-lg font-semibold">Files</h2>
+              <button onClick={handleCloseModal}>
+                <FaTimes />
+              </button>
+            </div>
+            <div className="space-y-2 grid gap-2">
+              {selectedFiles.map((file, index) => (
+                <a
+                  key={index}
+                  href={file.file}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-500 hover:underline  bg-gray-200 rounded p-2"
+                >
+                  Attachment {index + 1}
+                </a>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
